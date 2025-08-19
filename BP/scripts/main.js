d830e52ac3fe634b65050f7ad2282b43e4bf40b9
Taran Gauri <155413737+TaranGauri@users.noreply.github.com>
2025-08-19 13:03:04 +0000

    CombatEngineMCBE v0.0.1

---
+++ BP/scripts/main.js
@@ -0,0 +1,98 @@
+import { world, system, Player, ItemStack, EquipmentSlot } from "@minecraft/server";
+
+const config = {
+  horizontalKB: 0.2889,
+  verticalKB: 0.14,
+  sprintMultiplier: 1.05,
+  kbDelay: 2 //ticks
+};
+
+const weaponDamage = {
+  "minecraft:wooden_sword": 3,
+  "minecraft:stone_sword": 4,
+  "minecraft:golden_sword": 5,
+  "minecraft:iron_sword": 6,
+  "minecraft:diamond_sword": 7,
+  "minecraft:wooden_axe": 3,
+  "minecraft:stone_axe": 4,
+  "minecraft:golden_axe": 5,
+  "minecraft:iron_axe": 6,
+  "minecraft:diamond_axe": 7,
+  "minecraft:trident": 20
+};
+
+const armorReduction = {
+  "minecraft:leather_helmet": 0.04,
+  "minecraft:leather_chestplate": 0.12,
+  "minecraft:leather_leggings": 0.08,
+  "minecraft:leather_boots": 0.04,
+
+  "minecraft:iron_helmet": 0.08,
+  "minecraft:iron_chestplate": 0.24,
+  "minecraft:iron_leggings": 0.20,
+  "minecraft:iron_boots": 0.08,
+
+  "minecraft:diamond_helmet": 0.12,
+  "minecraft:diamond_chestplate": 0.32,
+  "minecraft:diamond_leggings": 0.24,
+  "minecraft:diamond_boots": 0.12
+};
+
+const kbCooldown = new Map();
+
+world.afterEvents.entityHitEntity.subscribe(({ damagingEntity, hitEntity }) => {
+  if (damagingEntity.getEffect("minecraft:weakness")) return; //can be removed safely to allow pvp while u have weakness effect
+
+  const mainHand = damagingEntity
+    .getComponent("minecraft:equippable")
+    ?.getEquipment(EquipmentSlot.Mainhand);
+
+  let baseDamage = weaponDamage[mainHand.typeId] || 1;
+
+  const armor = hitEntity.getComponent("minecraft:equippable");
+  let reduction = 0;
+
+  if (armor) {
+    const slots = [
+      EquipmentSlot.Head,
+      EquipmentSlot.Chest,
+      EquipmentSlot.Legs,
+      EquipmentSlot.Feet
+    ];
+
+    for (const slot of slots) {
+      const item = armor.getEquipment(slot);
+      if (!item || !(item.typeId in armorReduction)) continue;
+      reduction += armorReduction[item.typeId];
+    }
+  }
+
+  const finalDamage = baseDamage * (1 - reduction);
+  hitEntity.applyDamage(finalDamage);
+
+  const now = system.currentTick;
+  const lastKB = kbCooldown.get(hitEntity.id) || 0;
+  if (now - lastKB < config.kbDelay) return;
+  kbCooldown.set(hitEntity.id, now);
+
+  let victimLocation = hitEntity.location;
+  let attackerLocation = damagingEntity.location;
+  let horizontalKB = config.horizontalKB;
+  let verticalKB = config.verticalKB;
+
+  let knockbackX = victimLocation.x - attackerLocation.x;
+  let knockbackZ = victimLocation.z - attackerLocation.z;
+
+  const magnitude = Math.sqrt(knockbackX * knockbackX + knockbackZ * knockbackZ);
+  if (magnitude !== 0) {
+    knockbackX /= magnitude;
+    knockbackZ /= magnitude;
+  }
+
+  const horizontalForce = {
+    x: knockbackX * horizontalKB,
+    z: knockbackZ * horizontalKB
+  };
+
+  hitEntity.applyKnockback(horizontalForce, verticalKB * 1.023);
+});
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CombatEngineMCBE
 This is a combat engine, technically a beforeEvents for entityHurt or entityHitEntity, this system removes every vanilla pvp mechanics of minecraft bedrock.
+
+# Features
+- Weakness effect can stop PvP, so u can make no pvp zones.
+- Totally removes vanilla PvP, and makes a more customizable system.
+- Custom armor toughness can be defined.
+- Vanilla like knockback.
+- CPS limiter.
+
+# Things you have to edit for proper use
+- Armor toughness (they are not same as one on vanilla.)
+- Weapon damages (they are not same as the one on vanilla.)
+
+# Future updates
+- More damage with strength effect.
+- more damage with damage enchants.
